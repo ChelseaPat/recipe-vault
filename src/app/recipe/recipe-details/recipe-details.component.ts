f7269eda465061363f9faee8f9d40dd750d51014
waikato_cp77 <cp77@students.waikato.ac.nz>
2024-01-16 20:06:59 +1300

    On cancel make sure to reload page and remove changes

--- src/app/recipe/recipe-details/recipe-details.component.ts
+++ src/app/recipe/recipe-details/recipe-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EditModeService, EditSession } from 'src/app/common/edit-mode.service';
 import { Recipe, RecipeService } from 'src/app/recipe.service';
 
 @Component({
@@ -12,26 +13,47 @@ export class RecipeDetailsComponent implements OnInit {
 	id: string = '';
 	recipe: Recipe = null;
 	editMode = false;
+    editSession: EditSession = null;
 
 	MeasurementLabels = this.recipeService.MeasurementLabels;
 
     constructor(
+        public router: Router,
 		private route: ActivatedRoute,
-		private recipeService: RecipeService) {}
+		private recipeService: RecipeService,
+        private editModeService: EditModeService) {}
 
     ngOnInit (): void {
         this.id = this.route.snapshot.paramMap.get('id');
-		this.recipe = this.recipeService.getRecipeById(this.id);
+		this.recipe = JSON.parse(JSON.stringify(this.recipeService.getRecipeById(this.id)));
 
 		this.editMode = this.id === 'new';
+
+        this.editSession = {
+            docId: this.id,
+            docType: 'RECIPE' 
+        };
+
+        const boiii = this.editModeService.isAnEditSessionInProgress(this.editSession);
+        console.log('boi', boiii);
     }
 
 	startEditMode(): void {
-		this.editMode = true;
+        // this.editModeService.addAnEditSession(this.editSession);
+        this.editMode = true;
 	}
 
+    cancelEditMode(): void {
+        this.endEditMode();
+        const currentUrl = this.router.url;
+        this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+            this.router.navigate([currentUrl]);
+        });
+    }
+
 	endEditMode(): void {
-		this.editMode = false;
+		// this.editModeService.removeAnEditSession(this.editSession);
+        this.editMode = false;
 	}
 
 	onValueChange(value: string) {
