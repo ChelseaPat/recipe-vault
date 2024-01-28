149bab4aa60660fa2bb5d6589ceab4b5ccc44c7f
waikato_cp77 <cp77@students.waikato.ac.nz>
2024-01-28 20:54:45 +1300

    Clean up imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -16,12 +15,12 @@ import { HeaderComponent } from './core/header/header.component';
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent,
-        RecipeListComponent,
         ButtonComponent,
-        RecipeDetailsComponent,
         FieldComponent,
-        HeaderComponent
+        HeaderComponent,
+        HomeComponent,
+        RecipeDetailsComponent,
+        RecipeListComponent
     ],
     imports: [
         BrowserModule,
